e0ab8261d3d21d9e6b77df61527b553f098549f2
MiguelFD-byte <78120747+MiguelFD-byte@users.noreply.github.com>
2021-03-16 10:18:53 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   $('#check').click(function(){
+    $('.menu').slideToggle(300); 
+   }); 
+});
+
+$(document).ready(function(){
+   $('.content__banner').css('opacity','1'); 
+});
